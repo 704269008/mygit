b50b7f78b05a4b1e61ad5e620963229adf79bc74
second <39045654+704269008@users.noreply.github.com>
2018-08-19 13:25:19 +0800

    Add files via upload

--- task7/src/test/java/picture/uploadTest.java
+++ task7/src/test/java/picture/uploadTest.java
@@ -1,5 +1,6 @@
 package picture;
 
+import com.aliyun.oss.OSSClient;
 import com.jns.utils.OssDemo;
 
 import java.io.File;
@@ -7,7 +8,8 @@ import java.io.File;
 public class uploadTest {
     public static void main(String args[]){
         OssDemo ossDemo=new OssDemo();
+        OSSClient ossClient=OssDemo.getOssClient();
         //bucket名    自定义上传文件的名字，   文件在本地的具体路径，\ 用 \\代替
-        ossDemo.upload("does", "1.png", new File("D:\\kjk.png"));
+        ossDemo.upload(ossClient,"does", "1.png", new File("D:\\kjk.png"));
     }
 }
